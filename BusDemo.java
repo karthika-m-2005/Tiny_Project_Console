bd1224f0cfccd332fbaffd6cc9746262ee1fad89
KARTHIKA M <km646383@gmail.com>
2025-02-23 16:31:32 +0530

    Bus Reservation outline

---
+++ BusDemo.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class BusDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Bus> buses = new ArrayList<Bus>();
+		ArrayList<Booking> bookings = new ArrayList<Booking>();
+		
+		buses.add(new Bus(1,true,2));
+		buses.add(new Bus(2,false,9));
+		
+		int useroption = 1;
+		Scanner sc = new Scanner(System.in);
+		
+		for(Bus b :buses)
+		{
+			b.DisplayBusinfo();
+		}
+		
+		while(useroption == 1)
+		{
+		    System.out.print("Enter 1 to Book and 2 to Exit: ");
+		    useroption =  sc.nextInt();
+		    if(useroption == 1)
+		    {
+		    	Booking booking = new Booking();
+		    	if(booking.isAvailable(bookings,buses))
+		    	{
+		    		bookings.add(booking);
+		    		System.out.println("Your Booking is Confirmed");
+		    	}
+		    	else
+		    		System.out.println("Sorry! Bus is Full.Try Another Bus or Date ");
+		    }
+		}
+		
+		
+		
+
+	}
+
+}
