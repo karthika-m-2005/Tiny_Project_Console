775e90f37f0fd19675f460d296e86eb5edd2a911
KARTHIKA M <km646383@gmail.com>
2025-03-07 21:38:00 +0530

    update Booking Details

--- Bus.java
+++ Bus.java
@@ -3,12 +3,14 @@ public class Bus {
   private int Bus_no;
   private boolean Ac;
   private int Capacity;//get and set
+  private int Booking_Number;
   
-  Bus(int busno,boolean ac,int cap)
+  Bus(int busno,boolean ac,int cap,int b_no)
   {
 	  Bus_no = busno;
 	  Ac = ac;
 	  Capacity = cap;
+	  Booking_Number = b_no;
   }
   
   public int getCapcity() //acccessor method
@@ -35,11 +37,24 @@ public class Bus {
 	  return Bus_no;
 	  
   }
+ public int getBooking_Number()
+	{
+		return Booking_Number;
+	}
+ public void setBus_no()
+	{
+		 Bus_no = busno;
+	}
+ public int Booking_Number()
+	{
+		Booking_Number= b_no;
+	}
+		
   
   public void DisplayBusinfo()
   {
 	  System.out.println("------Bus Details--------");
-	  System.out.println("Bus NumberP:"+Bus_no+"\n"+"AC: "+Ac+"\n"+"Bus Capacity: "+Capacity);
+	  System.out.println("Bus NumberP:"+Bus_no+"\n"+"AC: "+Ac+"\n"+"Bus Capacity: "+Capacity+"\n"+"Booking Number"+Booking_Number);
 	  System.out.println("-------------------------");
   }
   
